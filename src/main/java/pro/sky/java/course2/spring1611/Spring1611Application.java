ce543d63f7d2047a5329c2d2467cab9f5413e2b4
MonakhovA <MonGraph67>
2022-11-18 20:03:17 +0300

    commit

---
+++ src/main/java/pro/sky/java/course2/spring1611/Spring1611Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.spring1611;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1611Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring1611Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/spring1611/Spring1611ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.spring1611;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1611ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
